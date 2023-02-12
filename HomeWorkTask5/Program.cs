aef9c08af0eda923c4920c81981b31ddcd73f649
Dmitry <marknmaxm@gmail.com>
2023-02-13 00:47:07 +0300

    Task5 ready

---
+++ HomeWorkTask5/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача # 5
+// Программа, которая на вход принимает число N, а на выходе показывает все чётные числа от 1 до N
+Console.Clear();
+Console.Write("Пожалуйста введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int temp = 2;
+while (temp <= N)
+{
+    Console.Write(temp + " ");
+    temp += 2;
+}
