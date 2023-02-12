97ab9e679024f102b90cf740ae458149b8b0a1ba
Dmitry <marknmaxm@gmail.com>
2023-02-12 17:00:38 +0300

    x^2

---
+++ Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программа, которая принимает одно число и выдаёт его квадрат.
+
+Console.Clear();
+Console.Write("Добро пожаловать! Введите число, для которого мы ищем квадрат: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine("Квадрат введённого числа = " + result);
+Console.WriteLine("Спасибо за использование приложения!");
