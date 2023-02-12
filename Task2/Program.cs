46794b0ccad5f92d0c0781d929a10a7954663aad
Dmitry <marknmaxm@gmail.com>
2023-02-12 23:11:34 +0300

    Работа на семинаре

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int FirstValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int SecondValue = Convert.ToInt32(Console.ReadLine());
+
+if (SecondValue * SecondValue == FirstValue)
+{
+    Console.Write("Число " + FirstValue + " является квадратом от " + SecondValue);
+}
+else
+{
+    Console.Write("Число " + FirstValue + " не является квадратом от " + SecondValue);
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая на входе принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+Console.Write("Введите число N : ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 0)
+{
+    N = N * (-1);
+}
+int negativeN = N * (-1);
+while (negativeN <= N)
+{
+    Console.Write(negativeN + " ");
+    negativeN += 1;
+}
