23c07cad1be33779e4e75e07ba024cc0290f4ba9
Dmitry <marknmaxm@gmail.com>
2023-02-13 00:03:44 +0300

    Task2 ready

--- HomeWorkTask2/Program.cs
+++ HomeWorkTask2/Program.cs
@@ -2,10 +2,10 @@
 // Программа, которая на вход принимает два числа и выдаёт, какое число больше, а какое меньше.
 Console.Clear();
 Console.Write("Уважаемый пользователь, представьтесь!");
-string username=Console.ReadLine();
-if(username.ToLower() == "михаил")
+string username = Console.ReadLine();
+if (username.ToLower() == "михаил")
 {
-   Console.WriteLine("Здравствуйте, Михаил! Большое спасибо за семинар, всё очень понятно и доступно объясняете");
+    Console.WriteLine("Здравствуйте, Михаил! Большое спасибо за семинар, всё очень понятно и доступно объясняете");
 }
 else
 {
@@ -13,24 +13,24 @@ else
     Console.WriteLine(username);
 }
 Console.Write("Пожалуйста введите первое число для сравнения: ");
-int FirstNum=Convert.ToInt32(Console.ReadLine());
+int FirstNum = Convert.ToInt32(Console.ReadLine());
 Console.Write("Пожалуйста введите второе число для сравнения: ");
-int SecondNum=Convert.ToInt32(Console.ReadLine());
+int SecondNum = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Идёт обратка запроса...");
 Console.WriteLine("Спасибо за ожидание");
-if (FirstNum==SecondNum)
+if (FirstNum == SecondNum)
 {
-    Console.WriteLine(username+", вы ввели равные числа, пожалуйста повторите попытку");
+    Console.WriteLine(username + ", вы ввели равные числа, пожалуйста повторите попытку");
 }
 else
 {
     if (FirstNum < SecondNum)
     {
-        Console.WriteLine(username + ", введённое вами второе число "+ "("+SecondNum+")"+ " больше первого " +"("+FirstNum+")");
+        Console.WriteLine(username + ", введённое вами второе число " + "(" + SecondNum + ")" + " больше первого " + "(" + FirstNum + ")");
     }
     else
     {
-        Console.WriteLine(username + ", введённое вами первое число "+ "("+FirstNum+")"+ " больше второго " +"("+SecondNum+")");
-        
+        Console.WriteLine(username + ", введённое вами первое число " + "(" + FirstNum + ")" + " больше второго " + "(" + SecondNum + ")");
+
     }
 }
