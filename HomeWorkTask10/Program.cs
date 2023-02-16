68408b99c2f742c70552f792130c8bafe134a928
Dmitry <marknmaxm@gmail.com>
2023-02-17 00:41:52 +0300

    HomeWork ready

---
+++ HomeWorkTask10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Уважаемый пользователь, введите трёхзначное число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int secondNum = 0;
+if (value >= 100 && value <= 999 || value >= -999 && value <= -100)
+{
+    secondNum = value / 10 % 10;
+    Console.WriteLine("Вторая цифра числа " + value + " равна: " + Math.Abs(secondNum));
+}
+else
+{
+    Console.Write("Уважаемый пользователь вы ввели некорректное число. Попробуйте еще раз");
+}
---
+++ HomeWorkTask13/Program.cs
@@ -0,0 +1,35 @@
+﻿// Программа, которая выводит третью цифру заданного семизначного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Уважаемый пользователь, введите семизначное число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int thirdNumL = 0;
+int thirdNumR = 0;
+string text = Convert.ToString(value);
+if (value <= 9999999 && value >= 1000000 || value >= -9999999 && value <= -1000000)
+{
+    thirdNumL = value / 10000 % 10;
+    thirdNumR = value / 100 % 10;
+    Console.WriteLine("Третья цифра числа " + value + " слева равна: " + Math.Abs(thirdNumL) + "," + " а справа равна: " + Math.Abs(thirdNumR));
+}
+else
+{
+    if (value >= -99 && value <= 99)
+    {
+        Console.WriteLine("Указанное вами чило не содержит третьей цифры");
+    }
+    else
+    {
+        if (value >= 100 && value <= 999999)
+        {
+            Console.WriteLine("Вы не соблюдаете первоначальное условие, но я всё равно постараюсь определить третью цифру...");
+            Console.WriteLine("Скорее всего, третья цифра вашего числа равна: " + text[2]);
+        }
+        else if (value >= -999999 && value <= -100)
+        {
+            Console.WriteLine("Вы не соблюдаете первоначальное условие, но я всё равно постараюсь определить третью цифру...");
+            Console.WriteLine("Скорее всего, третья цифра вашего числа равна: " + text[3]);
+
+        }
+    }
+
+}
